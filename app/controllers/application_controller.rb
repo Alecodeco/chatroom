ea75d5cdd979f515341f84aef991edafc72fbf83
Alejandro Codeseira <acodeseira@ATM.Local>
2020-01-29 14:57:35 -0300

    Session log in helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in."
+      redirect_to login_path
+    end
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "Must be logged in for that."
+      redirect_to root_path
+    end
+  end
+
 end
